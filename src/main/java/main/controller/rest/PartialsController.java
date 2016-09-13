e8fb8a0c490f044a879be69452aac2fae736c2db
ctirs <clemens.tirsch@web.de>
2016-09-13 17:40:17 +0200

    added partials controller

---
+++ src/main/java/main/controller/rest/PartialsController.java
@@ -0,0 +1,24 @@
+package main.controller.rest;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RestController
+public class PartialsController {
+
+    @RequestMapping(value = "/partials", method = RequestMethod.GET)
+    public String getPartial(@RequestParam(required = true, value = "partial") String partial) throws IOException, URISyntaxException {
+        return new String(Files.readAllBytes(
+                Paths.get(getClass().getClassLoader().getResource("templates/" + partial).toURI())), "UTF-8");
+    }
+
+
+}
