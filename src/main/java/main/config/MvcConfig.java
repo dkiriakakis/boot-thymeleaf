31f0fa17902b98fdfce0f2607dc462e1a2fdb3e6
ctirs <clemens.tirsch@web.de>
2016-09-13 17:39:48 +0200

    added spring security in order to set cache header

---
+++ src/main/java/main/config/MvcConfig.java
@@ -0,0 +1,40 @@
+package main.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Clemens Tirsch on 13.09.2016.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter
+         {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Resources controlled by Spring Security, which
+        // adds "Cache-Control: must-revalidate".
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/")
+                .setCachePeriod(3600*24);
+
+        registry.addResourceHandler("/bower_components/**")
+                .addResourceLocations("classpath:/static/bower_components/")
+                .setCachePeriod(3600*24);
+
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/")
+                .setCachePeriod(3600*24);
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/")
+                .setCachePeriod(3600*24);
+
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/")
+                .setCachePeriod(3600*24);
+    }
+}
---
+++ src/main/java/main/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package main.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.header.HeaderWriter;
+import org.springframework.security.web.header.writers.CacheControlHeadersWriter;
+import org.springframework.security.web.header.writers.DelegatingRequestMatcherHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
+
+/**
+ * Created by Clemens Tirsch on 13.09.2016.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().cacheControl().disable();
+       // http.headers().addHeaderWriter(new DelegatingRequestMatcherHeaderWriter( new NegatedRequestMatcher(new AntPathRequestMatcher("/images")), new CacheControlHeadersWriter() ));
+    }
+}
